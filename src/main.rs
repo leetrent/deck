fa3a3e10b9b39c7e718532151f2d960da296629b
LeeTrent <lee.trent.1@gmail.com>
2024-07-21 19:59:54 -0500

    Added 'new' function to create a Deck

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@
 struct Deck {
     cards: Vec<String>
 }
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-    let values = ["Ace", "King", "Queen", "Jack",  "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
-    
-    let mut cards = vec![];
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
 
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = ["Ace", "King", "Queen", "Jack",  "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
+        
+        let mut cards = vec![];
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+    
+            }
         }
+        Deck { cards }
     }
-    let deck: Deck = Deck { cards };
+}
+
+fn main() {
+    let deck = Deck::new();
     println!("Here is your deck: {:#?}", deck);
 }
